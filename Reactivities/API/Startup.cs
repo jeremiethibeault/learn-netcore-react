134cd189e5ebf4b367d40747197182d8598bdffa
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-02-21 12:24:09 -0500

    Adds axios for http calls.

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -31,6 +31,18 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -48,6 +60,9 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+
             app.UseMvc();
         }
     }
