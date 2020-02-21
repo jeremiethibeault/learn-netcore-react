c5a5d364f4c6981ee9f6d660aedf82a850a6db4b
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-02-21 14:43:01 -0500

    Adds strong typed in the React app.

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
-import './App.css';
 import axios from 'axios'
+import { IActivity } from '../models/activity';
 
-class App extends Component {
-  state = {
-    values: []
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+  readonly state: IState = {
+    activities: []
   }
 
   componentDidMount() {
     axios
-      .get("http://localhost:5000/api/values")
+      .get<IActivity[]>("http://localhost:5000/api/activities")
       .then((response) => {
         this.setState({
-          values: response.data
+          activities: response.data
         })
       })
   }
@@ -27,8 +31,8 @@ class App extends Component {
         </Header>
 
         <List>
-          {this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
+          {this.state.activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
       </div>
