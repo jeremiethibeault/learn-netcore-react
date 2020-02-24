91548f3cd27dbb36c4c6a771d412a72533e42d9c
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-02-24 18:36:06 -0500

    Adds item deletion.

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,6 +14,7 @@ interface IProps {
   setSelectedActivity: (activity: IActivity | null) => void;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  deleteActivity: (id: string) => void;
 }
 
 export const ActivityDashboard: React.FC<IProps> = ({
@@ -24,12 +25,17 @@ export const ActivityDashboard: React.FC<IProps> = ({
   setEditMode,
   setSelectedActivity,
   createActivity,
-  editActivity
+  editActivity,
+  deleteActivity
 }) => {
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} selectActivity={selectActivity} />
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+        />
       </Grid.Column>
 
       <Grid.Column width="6">
@@ -42,7 +48,7 @@ export const ActivityDashboard: React.FC<IProps> = ({
         )}
         {editMode && (
           <ActivityForm
-            key={selectedActivity && selectedActivity.id || 0}
+            key={(selectedActivity && selectedActivity.id) || 0}
             activity={selectedActivity}
             setEditMode={setEditMode}
             createActivity={createActivity}
