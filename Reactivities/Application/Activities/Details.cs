cba0371471cbc45668ce6e0b2bc17ab156eef6e9
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-03-26 19:13:42 -0400

    Adds error handling middleware on the API.

--- Reactivities/Application/Activities/Details.cs
+++ Reactivities/Application/Activities/Details.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
@@ -26,6 +28,11 @@ namespace Application.Activities
             public async Task<Activity> Handle(Query request, CancellationToken ct)
             {
                 var activity = await _context.Activities.FindAsync(keyValues: new object[] { request.Id }, cancellationToken: ct);
+
+                if (activity == null)
+                {
+                    throw new RestException(HttpStatusCode.NotFound, new { activity = "Not Found" });
+                }
 
                 return activity;
             }
--- Reactivities/Application/Activities/Edit.cs
+++ Reactivities/Application/Activities/Edit.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using FluentValidation;
 using MediatR;
 using Persistence;
@@ -38,23 +40,23 @@ namespace Application.Activities
                 RuleFor(x => x.Venue).NotEmpty();
             }
         }
-        
+
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            
+
             public Handler(DataContext context)
             {
                 _context = context;
             }
-            
+
             public async Task<Unit> Handle(Command request, CancellationToken ct)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
 
                 if (activity == null)
                 {
-                    throw new Exception("Could not find activity.");
+                    throw new RestException(HttpStatusCode.NotFound, new { activity = "Not Found" });
                 }
 
                 activity.Title = request.Title ?? activity.Title;
@@ -63,14 +65,14 @@ namespace Application.Activities
                 activity.Date = request.Date ?? activity.Date;
                 activity.City = request.City ?? activity.City;
                 activity.Venue = request.Venue ?? activity.Venue;
-        
+
                 var success = await _context.SaveChangesAsync(ct) > 0;
-        
+
                 if (success)
                 {
                     return Unit.Value;
                 }
-        
+
                 throw new Exception("Problem saving changes.");
             }
         }
