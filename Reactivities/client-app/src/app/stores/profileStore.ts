c9dbba0489ec846d123546499260b34c433fef9e
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-07-07 21:13:55 -0400

    Adds upload, set main and delete of photos. Adds target pattern to avoid sharing loading states

--- Reactivities/client-app/src/app/stores/profileStore.ts
+++ Reactivities/client-app/src/app/stores/profileStore.ts
@@ -1,7 +1,8 @@
 import { RootStore } from "./rootStore";
 import { observable, action, runInAction, computed } from "mobx";
-import { IProfile } from "../models/profile";
+import { IProfile, IPhoto } from "../models/profile";
 import agent from "../api/agent";
+import { toast } from "react-toastify";
 
 export default class ProfileStore {
   rootStore: RootStore;
@@ -12,6 +13,8 @@ export default class ProfileStore {
 
   @observable profile: IProfile | null = null;
   @observable loadingProfile = true;
+  @observable uploadingPhoto = false;
+  @observable loading = false;
 
   @computed get isCurrentUser() {
       if (this.rootStore.userStore.user && this.profile) {
@@ -37,4 +40,68 @@ export default class ProfileStore {
       console.log(error);
     }
   };
+
+  @action uploadPhoto = async (file: Blob) => {
+    this.uploadingPhoto = true;
+    try {
+      const photo = await agent.Profiles.uploadPhoto(file);
+      runInAction(() => {
+        if (this.profile) {
+          this.profile.photos.push(photo);
+
+          if (photo.isMain && this.rootStore.userStore.user) {
+            this.rootStore.userStore.user.image = photo.url;
+            this.profile.image = photo.url;
+          }
+
+          this.uploadingPhoto = false;
+        }
+      })
+    } catch (error) {
+        console.log(error);
+        toast.error("Problem uploading photo");
+        runInAction(() => {
+          this.uploadingPhoto = false;
+        })
+    }
+  }
+
+  @action setMainPhoto = async (photo: IPhoto) => {
+    this.loading = true;
+    try {
+      await agent.Profiles.setMainPhoto(photo.id);
+
+      runInAction(() => {
+        this.rootStore.userStore.user!.image = photo.url;
+        this.profile!.photos.find(a => a.isMain)!.isMain = false;
+        this.profile!.photos.find(a => a.id === photo.id)!.isMain = true;
+        this.profile!.image = photo.url;
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error("Problem setting main photo");
+      runInAction(() => {
+        this.loading = false;
+      })
+    }
+  }
+
+  @action deletePhoto = async (photo: IPhoto) => {
+    this.loading = true;
+    try {      
+      await agent.Profiles.deletePhoto(photo.id);
+
+      runInAction(() => {
+        this.profile!.photos = this.profile!.photos.filter(a => a.id !== photo.id);
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error("Problem deleting photo");
+      runInAction(() => {
+        this.loading = false;
+      })
+    }
+  }
 }
--- Reactivities/client-app/src/features/profiles/ProfilePhotos.tsx
+++ Reactivities/client-app/src/features/profiles/ProfilePhotos.tsx
@@ -2,11 +2,28 @@ import React, { useContext, useState } from "react";
 import { Tab, Header, Card, Image, Button, Grid } from "semantic-ui-react";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import PhotoUploadWidget from "../../app/common/photoUpload/PhotoUploadWidget";
+import { observer } from "mobx-react-lite";
 
 const ProfilePhotos = () => {
   const rootStore = useContext(RootStoreContext);
-  const { profile, isCurrentUser } = rootStore.profileStore;
-  const [addPhotoMode, setAddPhotoMode] = useState(true);
+  const {
+    profile,
+    isCurrentUser,
+    uploadPhoto,
+    uploadingPhoto,
+    setMainPhoto,
+    loading,
+    deletePhoto,
+  } = rootStore.profileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const [target, setTarget] = useState<string | undefined>(undefined);
+  const [deleteTarget, setDeleteTarget] = useState<string | undefined>(
+    undefined
+  );
+
+  const handleUploadImage = (photo: Blob) => {
+    uploadPhoto(photo).then(() => setAddPhotoMode(false));
+  };
 
   return (
     <Tab.Pane>
@@ -24,19 +41,44 @@ const ProfilePhotos = () => {
         </Grid.Column>
         <Grid.Column width={16}>
           {addPhotoMode ? (
-            <PhotoUploadWidget />
+            <PhotoUploadWidget
+              uploadPhoto={handleUploadImage}
+              loading={uploadingPhoto}
+            />
           ) : (
             <Card.Group itemsPerRow={5}>
               {profile &&
                 profile.photos.map((photo) => (
                   <Card key={photo.id}>
                     <Image src={photo.url} />
-                    {isCurrentUser && 
+                    {isCurrentUser && (
                       <Button.Group fluid width={2}>
-                        <Button basic positive content="Main" />
-                        <Button basic negative icon="trash" />
+                        <Button
+                          name={photo.id}
+                          basic
+                          positive
+                          content="Main"
+                          onClick={(e) => {
+                            setTarget(e.currentTarget.name);
+                            setMainPhoto(photo);
+                          }}
+                          loading={loading && target === photo.id}
+                          disabled={photo.isMain}
+                        />
+                        <Button
+                          name={photo.id}
+                          basic
+                          negative
+                          icon="trash"
+                          disabled={photo.isMain}
+                          onClick={(e) => {
+                            setDeleteTarget(e.currentTarget.name);
+                            deletePhoto(photo);
+                          }}
+                          loading={loading && deleteTarget === photo.id}
+                        />
                       </Button.Group>
-                    }
+                    )}
                   </Card>
                 ))}
             </Card.Group>
@@ -47,4 +89,4 @@ const ProfilePhotos = () => {
   );
 };
 
-export default ProfilePhotos;
+export default observer(ProfilePhotos);
