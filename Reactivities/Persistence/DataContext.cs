f5c661753695c6ab47bb07cad34324eebd8a84ed
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-07-09 20:55:37 -0400

    Adds self referencing many-to-many table.

--- Reactivities/Persistence/DataContext.cs
+++ Reactivities/Persistence/DataContext.cs
@@ -10,7 +10,7 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        
+
         public DbSet<Value> Values { get; set; }
 
         public DbSet<Activity> Activities { get; set; }
@@ -21,19 +21,22 @@ namespace Persistence
 
         public DbSet<Comment> Comments { get; set; }
 
+        public DbSet<UserFollowing> Followings { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<Value>()
                 .HasData(
-                    new Value {Id = 1, Name = "Value 101"},
-                    new Value {Id = 2, Name = "Value 102"},
-                    new Value {Id = 3, Name = "Value 103"}
+                    new Value { Id = 1, Name = "Value 101" },
+                    new Value { Id = 2, Name = "Value 102" },
+                    new Value { Id = 3, Name = "Value 103" }
                 );
 
             builder.Entity<UserActivity>(x => x
-                .HasKey(ua => new {
+                .HasKey(ua => new
+                {
                     ua.AppUserId,
                     ua.ActivityId
                 })
@@ -50,6 +53,21 @@ namespace Persistence
                 .WithMany(activity => activity.UserActivities)
                 .HasForeignKey(userActivity => userActivity.ActivityId)
             );
+
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new { k.ObserverId, k.TargetId });
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
